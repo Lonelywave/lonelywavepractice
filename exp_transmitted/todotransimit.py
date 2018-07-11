2c62bbc65b7118042719abd405c350d404d116c7
lonelywave <yuhsienlo@gmail.com>
2018-07-11 12:03:25 +0800

    transmit break for loop

--- exp_transmitted/todotransimit.py
+++ exp_transmitted/todotransimit.py
@@ -10,25 +10,29 @@ import time
 import sys
 
 def monitorCopy(diskname):
-
 	cmd = 'dir /b ' + diskname + '\\'
 	baselineFiles = check_output(cmd, shell=True).decode(sys.stdout.encoding).strip().split()
 
 	while True:
 		time.sleep(1)
-		monitorFiles = check_output(cmd, shell=True).decode(sys.stdout.encoding).strip().split()
-		if len(baselineFiles) != len(monitorFiles):
-			print(list(set(monitorFiles) - set(baselineFiles)))
-			copiedFilename = list(set(monitorFiles) - set(baselineFiles))[0]
-			unzipcmd = '7z.exe x ' + diskname + '\\' + copiedFilename + ' -oC:\\Workspace\\NTUST\\isLab\\exp2\\temp\\'
-			check_output(unzipcmd, shell=True).decode(sys.stdout.encoding)
-			print ('unzip')
-			cmdnc = 'nc.exe 192.168.72.159 8088 < C:\\Workspace\\NTUST\\isLab\\exp2\\temp\\word\\media\\image1.png'
-			check_output(cmdnc, shell=True).decode(sys.stdout.encoding)
-			print('Transmit')
-		else:
-			print('C')
-			
+		try:
+			monitorFiles = check_output(cmd, shell=True).decode(sys.stdout.encoding).strip().split()
+			if len(baselineFiles) < len(monitorFiles):
+				print(list(set(monitorFiles) - set(baselineFiles)))
+				copiedFilename = list(set(monitorFiles) - set(baselineFiles))[0]
+				unzipcmd = '7z.exe x ' + diskname + '\\' + copiedFilename + ' -oC:\\Workspace\\NTUST\\isLab\\exp2\\temp\\'
+				check_output(unzipcmd, shell=True).decode(sys.stdout.encoding)
+				print ('unzip')
+				cmdnc = 'nc.exe 192.168.72.159 8088 < C:\\Workspace\\NTUST\\isLab\\exp2\\temp\\word\\media\\image1.png'
+				check_output(cmdnc, shell=True).decode(sys.stdout.encoding)
+				print('Transmit')
+				baselineFiles = check_output(cmd, shell=True).decode(sys.stdout.encoding).strip().split()
+			else:
+				print('Not Docu Transmit in')
+		except:
+			break
+		
+		
 def main():
 	baseline = str(check_output("wmic  logicaldisk get name", shell=True).decode()).strip().split()
 	
@@ -36,10 +40,10 @@ def main():
 		time.sleep(1)
 		monitor = str(check_output("wmic  logicaldisk get name", shell=True).decode()).strip().split()
 		if len(baseline) != len(monitor):
-			print ('B')
+			print ('Disk changed')
 			monitorCopy(monitor[-1])
 		else:
-			print ('A')
+			print ('Disk not changed')
 				
 if __name__ == '__main__':
     main()
